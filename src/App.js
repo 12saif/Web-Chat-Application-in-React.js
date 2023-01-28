20a2210cfe7b5a25097e0f6928b3c74787b4a845
md saif <mohdsaif9170613062@gmail.com>
2023-01-28 20:32:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/ChatFeed'
+import "./App.css";
+// import { render } from "@testing-library/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="b11bea1a-e47f-459f-ba3d-fdd4ac1525b6"
+      userName="mdsaif12"
+      userSecret="Saif1234"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,9 @@
+const ChatFeed = (props) => {
+    console.log(props)
+  return (
+    <div>
+        ChatFeed Saif
+    </div>
+  )
+}
+export default ChatFeed;
